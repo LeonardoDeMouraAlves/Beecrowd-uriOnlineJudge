74fedebdc81a2db865cdf30ffe088cdc76ad452b
Leonardo de Moura Alves <163797008+LeonardoDeMouraAlves@users.noreply.github.com>
2024-03-22 17:48:53 -0300

    beecrowd | 2755 Saída 9

---
+++ C#/2755.cs
@@ -0,0 +1,17 @@
+using System;
+
+//Desenvolvido por Leonardo de Moura Alves
+
+class URI {
+
+    static void Main(string[] args) { 
+    
+        string frase = "\"Ro'b'er\tto\\/\"";
+        string frase1 = "(._.) ( l: ) ( .-. ) ( :l ) (._.)";
+        string frase2 = "(^_-) (-_-) (-_^)";
+        string frase3 = "(\"_\") ('.')";
+        
+        Console.Write(frase + "\n" + frase1 + "\n" + frase2 + "\n" + frase3 + "\n");
+        
+    }
+}

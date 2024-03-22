448b669fac0057ac5c48405fc1b6974daae9ccab
Leonardo de Moura Alves <163797008+LeonardoDeMouraAlves@users.noreply.github.com>
2024-03-22 12:40:24 -0300

    beecrowd | 1017 Gasto de Combustível

---
+++ C#/1017.cs
@@ -0,0 +1,13 @@
+using System;
+
+//Desenvolvido por Leonardo de Moura Alves
+
+class URI {
+
+    static void Main(string[] args) { 
+    
+        float x = float.Parse(Console.ReadLine()), y = float.Parse(Console.ReadLine());
+        Console.Write("{0:f3}\n",x*y/12);
+        
+    }
+}

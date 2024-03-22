181f5cf2757c01d1604aae558f6717edc11411e2
Leonardo de Moura Alves <163797008+LeonardoDeMouraAlves@users.noreply.github.com>
2024-03-22 12:04:28 -0300

    beecrowd | 1011 Esfera

--- C#/1011.cs
+++ C#/1011.cs
@@ -2,10 +2,9 @@ using System;
 
 //Desenvolvido por Leonardo de Moura Alves
 
-public class HelloWorld
-{
-    public static void Main(string[] args)
-    {
+class URI {
+
+    static void Main(string[] args) { 
         double raio = Convert.ToDouble(Console.ReadLine());
         
         Console.Write("VOLUME = {0}\n", (Convert.ToDouble(4.0/3.0)*3.14159*raio*raio*raio).ToString("0.000"));

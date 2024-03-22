a72906a21fb867186f55a616ee9d966323cbf0f0
Leonardo de Moura Alves <163797008+LeonardoDeMouraAlves@users.noreply.github.com>
2024-03-22 12:37:10 -0300

    beecrowd | 1016 Distância

---
+++ C#/1016.cs
@@ -0,0 +1,13 @@
+using System;
+
+//Desenvolvido por Leonardo de Moura Alves
+
+class URI {
+
+    static void Main(string[] args) { 
+    
+        int x = Convert.ToInt32(Console.ReadLine());
+        
+        Console.Write("{0} minutos\n", x*2);
+    }
+}
